fdd160d45368dec35c8af091c9eb03558a2c7186
深渊口香糖 <5183454+yuanjianming666@user.noreply.gitee.com>
2024-09-26 10:21:52 +0800

    fix：update

---
+++ smartrm-monolith/src/test/java/com/smartrm/smartrmmonolith/event/TestEvent.java
@@ -0,0 +1,15 @@
+package com.smartrm.smartrmmonolith.event;
+
+import com.smartrm.smartrmmonolith.infracore.event.DomainEvent;
+
+/**
+ * @author: yoda
+ * @description:
+ */
+public class TestEvent extends DomainEvent {
+
+
+  public TestEvent() {
+    super("test.event");
+  }
+}
---
+++ smartrm-monolith/src/test/java/com/smartrm/smartrmmonolith/event/TestEvent1.java
@@ -0,0 +1,14 @@
+package com.smartrm.smartrmmonolith.event;
+
+import com.smartrm.smartrmmonolith.infracore.event.DomainEvent;
+
+/**
+ * @author: yoda
+ * @description:
+ */
+public class TestEvent1 extends DomainEvent {
+
+  public TestEvent1() {
+    super("test.event1");
+  }
+}
